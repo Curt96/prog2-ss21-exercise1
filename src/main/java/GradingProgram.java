82e306dccbb81d4dd8dfc49e6907b40a63105b96
Curt <curt.schleritzko@fhcampuswien.ac.at>
2022-03-21 18:44:15 +0100

    roundGrade Method added

--- src/main/java/GradingProgram.java
+++ src/main/java/GradingProgram.java
@@ -12,37 +12,24 @@ public class GradingProgram {
 
     protected static List<Integer> gradingStudents(List<Integer> grades) {
         List<Integer> studentsGrades = new ArrayList<>();
-        for (Integer i : grades) {
-            System.out.println(i + " Vor dem if");
-            Integer singleGrade = i;
-            if (singleGrade > 37 && (singleGrade % 5 >= 3)) { //wenn es näher zu 5 ist aufrunden
-                System.out.println(singleGrade + " Im if vor dem runden");
-                singleGrade = singleGrade + (5 - singleGrade % 5); //53 % 5 = 3 -> 53 + ( 5 - 3(=actual[i] %5)  = 55
-                System.out.println(singleGrade + " nach dem runden");
-            }
-                else {
-                System.out.println(singleGrade + " Im else");
-            }
-            studentsGrades.add(singleGrade);
+        for (Integer index : grades) {
+            System.out.println(index + " Vor dem if");
+            studentsGrades.add(roundGrade(index));
         }
         return studentsGrades;
-   /* public static int[] roundGrade(int[] actual) {
-        for (int i = 0; i < actual.length; i++) {
-            System.out.println("for dem if " + actual[i]);
-            if (actual[i] > 37 && (actual[i] %5 >= 3)){ //wenn es näher zu 5 ist aufrunden
-                System.out.println("Im ersten if vor dem runden " + actual[i]);
-                actual[i] = actual[i] + (5 - actual[i] % 5); //53 % 5 = 3 -> 53 + ( 5 - 3(=actual[i] %5)  = 55
-                System.out.println("nach dem runden " + actual[i]);
-            }
-            /*if (actual[i] > 37 && ((actual[i] %5 ==2 || actual[i] %5 ==1))){ //wenn es näher zu 0 ist abrunden
-                System.out.println("im zweiten if ");
-                actual[i] = actual[i] - actual[i] %5;
-                System.out.println("nach runden im 2ten if " + actual[i]);
-            }
+    }
+
+    protected static Integer roundGrade(Integer singleGrade) {
+        int finaleGrade = singleGrade;
+        if (singleGrade > 37 && (singleGrade % 5 >= 3)) { //wenn es näher zu 5 ist aufrunden
+            System.out.println(singleGrade + " Im if vor dem runden");
+            finaleGrade += (5 - singleGrade % 5); //53 % 5 = 3 -> 53 + ( 5 - 3(=actual[i] %5)  = 55
+            System.out.println(singleGrade + " nach dem runden");
+        } else {
+            System.out.println(singleGrade + " Im else");
         }
-        return actual;
-    } */
-        }
+        return finaleGrade;
+    }
 
     public static Object totalStudent(List<Integer> studentsAndGrades) {
         Integer students;
